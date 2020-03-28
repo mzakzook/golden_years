6e5d750d1d368885b7c009363d5b57b8b42f9806
Max Zakzook <mzakzook@me.com>
2020-03-28 10:49:40 -0700

    Fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_03_28_173858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "volunteers", force: :cascade do |t|
+    t.string "name"
+    t.string "bio"
+    t.string "city"
+    t.string "state"
+    t.string "linkedin"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
